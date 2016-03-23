84ff5e61e17e1aa94091e814f56bc1426337315b
James&Lei <J&L@appacademy.io>
2016-03-23 12:06:43 -0400

    complete phase 2

---
+++ frontend/OrganGrinder.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var root = document.getElementById('root');
+Dispatcher = require('./dispatcher/Dispatcher');
+note = require('./util/Note');
+
+// ReactDOM.render(<TodoList />, root);
---
+++ frontend/constants/Tones.js
@@ -0,0 +1,7 @@
+var TONES = {
+  c4: 262,
+  CS4: 277,
+  D4: 293
+};
+
+module.exports = TONES;
